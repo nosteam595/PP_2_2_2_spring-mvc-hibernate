7af6f3d9ec227de8ebcc83f1fd8e47b14da0fd61
nosteam595 <nosteam@bk.ru>
2026-02-11 12:19:53 +0300

    Commit 8

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,6 +1,9 @@
 package web.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -19,11 +22,18 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
         };
     }
 
-
     /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    //Нормальное отображение кириллице при добавлении в БД
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[]{characterEncodingFilter};
     }
 
 }
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,10 +1,36 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getAllUsers(ModelMap modelMap) {
+        modelMap.addAttribute("users", userService.allUsers());
+        return "users";
+    }
+
+    @GetMapping("/add")
+    public String addNewUser(ModelMap modelMap) {
+        modelMap.addAttribute("user", new User());
+        return "userAdd";
+    }
+
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
 }
